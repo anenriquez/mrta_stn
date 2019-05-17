ff593d94c784fc13f8e062aea831bdf55edf5ada
Angela Enriquez <anenriqu@outlook.com>
2019-05-17 11:10:08 +0200

    Adding requirements and setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='multi-robot-task-allocation-uncertainties',
+      version='0.1.0',
+      description='Multi-robot task allocation with temporal constraints and uncertain durations',
+      author='Angela Enriquez Gomez',
+      author_email='angela.enriquez@smail.inf.h-brs.de',
+      package_dir={'': '.'}
+      )
