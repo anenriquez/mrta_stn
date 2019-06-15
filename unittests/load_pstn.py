bda4db1b07548e9e19f6843722b6f4e4ec391b24
Angela Enriquez <anenriqu@outlook.com>
2019-06-15 12:13:39 +0200

    Updating tests for PSTN using the new structure

--- unittests/load_pstn.py
+++ unittests/load_pstn.py
@@ -8,10 +8,9 @@ from scheduler.scheduler import Scheduler
 STN = "data/pstn_two_tasks.json"
 
 
-class TestBuildPSTN(object):
+class TestBuildPSTN(unittest.TestCase):
 
-    def __init__(self):
-
+    def setUp(self):
         # Load the stn as a dictionary
         with open(STN) as json_file:
             pstn_dict = json.load(json_file)
@@ -32,20 +31,77 @@ class TestBuildPSTN(object):
         print(guide_stn)
         print("Alpha: ", alpha)
 
-        # metric, minimal_network = self.scheduler.get_dispatch_graph()
-        #
-        # print("Minimal STN: \n", minimal_network)
-        #
-        # completion_time = minimal_network.get_completion_time()
-        # makespan = minimal_network.get_makespan()
-        # print("Completion time: ", completion_time)
-        # print("Makespan: ", makespan)
-        #
-        # self.assertEqual(completion_time, 107)
-        # self.assertEqual(makespan, 107)
+        completion_time = guide_stn.get_completion_time()
+        makespan = guide_stn.get_makespan()
+        print("Completion time: ", completion_time)
+        print("Makespan: ", makespan)
+
+        self.assertEqual(completion_time, 70)
+        self.assertEqual(makespan, 107)
+
+        expected_alpha = 0.023
+        self.assertEqual(alpha, expected_alpha)
+
+        contingent_constraints = guide_stn.get_contingent_constraints()
+
+        for (i, j), constraint in sorted(contingent_constraints.items()):
+
+            if i == 0 and j == 1:
+                lower_bound = -guide_stn[j][i]['weight']
+                upper_bound = guide_stn[i][j]['weight']
+                self.assertEqual(lower_bound, 37)
+                self.assertEqual(upper_bound, 38)
+            if i == 0 and j == 2:
+                lower_bound = -guide_stn[j][i]['weight']
+                upper_bound = guide_stn[i][j]['weight']
+                self.assertEqual(lower_bound, 41)
+                self.assertEqual(upper_bound, 46)
+            if i == 0 and j == 3:
+                lower_bound = -guide_stn[j][i]['weight']
+                upper_bound = guide_stn[i][j]['weight']
+                self.assertEqual(lower_bound, 41)
+                self.assertEqual(upper_bound, 52)
+            if i == 0 and j == 4:
+                lower_bound = -guide_stn[j][i]['weight']
+                upper_bound = guide_stn[i][j]['weight']
+                self.assertEqual(lower_bound, 90)
+                self.assertEqual(upper_bound, 91)
+            if i == 0 and j == 5:
+                lower_bound = -guide_stn[j][i]['weight']
+                upper_bound = guide_stn[i][j]['weight']
+                self.assertEqual(lower_bound, 96)
+                self.assertEqual(upper_bound, 101)
+            if i == 0 and j == 6:
+                lower_bound = -guide_stn[j][i]['weight']
+                upper_bound = guide_stn[i][j]['weight']
+                self.assertEqual(lower_bound, 96)
+                self.assertEqual(upper_bound, 107)
+            if i == 1 and j == 2:
+                lower_bound = -guide_stn[j][i]['weight']
+                upper_bound = guide_stn[i][j]['weight']
+                self.assertEqual(lower_bound, 0)
+                self.assertEqual(upper_bound, 47)
+            if i == 2 and j == 3:
+                lower_bound = -guide_stn[j][i]['weight']
+                upper_bound = guide_stn[i][j]['weight']
+                self.assertEqual(lower_bound, 0)
+                self.assertEqual(upper_bound, 11)
+            if i == 3 and j == 4:
+                lower_bound = -guide_stn[j][i]['weight']
+                upper_bound = guide_stn[i][j]['weight']
+                self.assertEqual(lower_bound, 0)
+                self.assertEqual(upper_bound, 61)
+            if i == 4 and j == 5:
+                lower_bound = -guide_stn[j][i]['weight']
+                upper_bound = guide_stn[i][j]['weight']
+                self.assertEqual(lower_bound, 0)
+                self.assertEqual(upper_bound, 61)
+            if i == 5 and j == 6:
+                lower_bound = -guide_stn[j][i]['weight']
+                upper_bound = guide_stn[i][j]['weight']
+                self.assertEqual(lower_bound, 0)
+                self.assertEqual(upper_bound, 11)
 
 
 if __name__ == '__main__':
-    # unittest.main()
-    test = TestBuildPSTN()
-    test.test_build_stn()
+    unittest.main()
--- unittests/update_pstn.py
+++ unittests/update_pstn.py
@@ -5,11 +5,11 @@ import collections
 from scheduler.structs.task import Task
 from scheduler.temporal_networks.pstn import PSTN
 
-DATASET = "data/two_tasks.yaml"
+DATASET = "data/three_tasks.yaml"
 
 
-class TestBuildPSTN():
-    def __init__(self):
+class TestBuildPSTN(unittest.TestCase):
+    def setUp(self):
         self.tasks = self.get_tasks()
 
     def get_tasks(self):
@@ -43,40 +43,165 @@ class TestBuildPSTN():
         print("N edges: ", n_edges)
         print(pstn)
 
-        contingent_constraints = pstn.get_contingent_constraints()
-        print(contingent_constraints)
+        # contingent_constraints = ppstn.get_contingent_constraints()
+        # print(contingent_constraints)
+        #
+        # ppstn_json = ppstn.to_json()
+        # print("Json format")
+        # print(ppstn_json)
 
-        pstn_json = pstn.to_json()
-        print("Json format")
-        print(pstn_json)
+        self.assertEqual(n_nodes, pstn.number_of_nodes())
+        self.assertEqual(n_edges, pstn.number_of_edges())
 
-        # self.assertEqual(n_nodes, stn.number_of_nodes())
-        # self.assertEqual(n_edges, stn.number_of_edges())
+    def test_add_task_beggining(self):
+        """Adds task at the beginning. Displaces the other tasks
+        """
+        print("--->Adding task at the beginning...")
+        pstn = PSTN()
+        # Adds the first task in position 1
+        pstn.add_task(self.tasks[0], 1)
+        print(pstn)
 
-    # def test_build_stn(self):
-    #     print("STN: \n", self.scheduler.temporal_network)
-    #     # print("Nodes: ", self.pstn.nodes.data())
-    #     # print("Edges: ", self.pstn.edges.data())
-    #     # print("Contingent constraints: ", self.pstn.contingent_constraints)
+        # Adds a new task in position 1. Displaces existing task at position 1 to position 2
+        pstn.add_task(self.tasks[1], 1)
+        print(pstn)
+
+        # We added two tasks
+        added_tasks = [self.tasks[0], self.tasks[1]]
+        n_nodes = 3 * len(added_tasks) + 1
+        n_edges = 2 * (5 * len(added_tasks) + len(added_tasks)-1)
+
+        print("N nodes: ", n_nodes)
+        print("N edges: ", n_edges)
+        print(pstn)
+
+        self.assertEqual(n_nodes, pstn.number_of_nodes())
+        self.assertEqual(n_edges, pstn.number_of_edges())
+
+    def test_add_task_middle(self):
+        print("--->Adding task in the middle...")
+        pstn = PSTN()
+        # Add task in position 1
+        pstn.add_task(self.tasks[0], 1)
+
+        # Adds task in position 2.
+        pstn.add_task(self.tasks[1], 2)
+        print(pstn)
+
+        # Add a new task in position 2. Displace existing task at position 2 to position 3
+        pstn.add_task(self.tasks[2], 2)
+        print(pstn)
+
+        n_nodes = 3 * len(self.tasks) + 1
+        n_edges = 2 * (5 * len(self.tasks) + len(self.tasks)-1)
+
+        print("N nodes: ", n_nodes)
+        print("N edges: ", n_edges)
+
+        self.assertEqual(n_nodes, pstn.number_of_nodes())
+        self.assertEqual(n_edges, pstn.number_of_edges())
+
+    def test_remove_task_beginning(self):
+        print("--->Removing task at the beginning...")
+        pstn = PSTN()
+
+        # Add all tasks
+        for i, task in enumerate(self.tasks):
+            pstn.add_task(task, i+1)
+
+        # Remove task in position 1
+        pstn.remove_task(1)
+
+        added_tasks = self.tasks[:-1]
+        n_nodes = 3 * len(added_tasks) + 1
+        n_edges = 2 * (5 * len(added_tasks) + len(added_tasks)-1)
+
+        print("N nodes: ", n_nodes)
+        print("N edges: ", n_edges)
+        print(pstn)
+
+        self.assertEqual(n_nodes, pstn.number_of_nodes())
+        self.assertEqual(n_edges, pstn.number_of_edges())
+
+    def test_remove_task_middle(self):
+        print("--->Removing task in the middle...")
+        pstn = PSTN()
+
+        # Add all tasks
+        for i, task in enumerate(self.tasks):
+            pstn.add_task(task, i+1)
+        print(pstn)
+
+        # Remove task in position 2
+        pstn.remove_task(2)
+
+        added_tasks = self.tasks[:-1]
+        n_nodes = 3 * len(added_tasks) + 1
+        n_edges = 2 * (5 * len(added_tasks) + len(added_tasks)-1)
+
+        print("N nodes: ", n_nodes)
+        print("N edges: ", n_edges)
+        print(pstn)
+
+        self.assertEqual(n_nodes, pstn.number_of_nodes())
+        self.assertEqual(n_edges, pstn.number_of_edges())
+
+    def test_remove_task_end(self):
+        print("--->Removing task at the end...")
+        pstn = PSTN()
+
+        # Add all tasks
+        for i, task in enumerate(self.tasks):
+            pstn.add_task(task, i+1)
+            print(pstn)
+
+        print(pstn)
+        # Remove task in position 3
+        pstn.remove_task(3)
+
+        added_tasks = self.tasks[:-1]
+        n_nodes = 3 * len(added_tasks) + 1
+        n_edges = 2 * (5 * len(added_tasks) + len(added_tasks)-1)
+
+        print("N nodes: ", n_nodes)
+        print("N edges: ", n_edges)
+        print(pstn)
+
+        # data1 = json_graph.node_link_data(pstn)
+        # MyEncoder().encode(data1)
+        # print(data1)
+        # print("------------")
+        # s1 = json.dumps(data1, cls=MyEncoder)
+        # print(s1)
+        # pstn1 = json.loads(data1)
+        # print(s1)
+        # print(json_graph.node_link_graph(data1, directed=True))
+        # print("----", H)
+
+        self.assertEqual(n_nodes, pstn.number_of_nodes())
+        self.assertEqual(n_edges, pstn.number_of_edges())
+
+    # def test_add_two_tasks(self):
+    #     print("----Adding two tasks")
+    #     pstn = PSTN()
+    #     # Add task in position 1
+    #     pstn.add_task(self.tasks[1], 1)
     #
-    #     # minimal_stnu = self.pstn.floyd_warshall()
-    #     # self.pstn.update_edges(minimal_stnu)
-    #     # minimal_network = get_minimal_network(self.pstn)
+    #     # Adds task in position 2.
+    #     pstn.add_task(self.tasks[0], 2)
+    #     print(pstn)
     #
-    #     alpha, guide = self.scheduler.get_dispatch_graph()
-    #     print("Schedule: \n", guide)
-    #
-    #     completion_time = guide.get_completion_time()
-    #     makespan = guide.get_makespan()
-    #
-    #     print("Completion time: ", completion_time)
-    #     print("Makespan: ", makespan)
-    #
-    #     self.assertEqual(completion_time, 69)
-    #     self.assertEqual(makespan, 107)
+    #     # pstn_json = pstn.to_json()
+    #     # print("JSON format")
+    #     # print(pstn_json)
 
 
 if __name__ == '__main__':
-    # unittest.main
-    test = TestBuildPSTN()
-    test.test_add_tasks_consecutively()
+    unittest.main()
+    # test = TestBuildPpstn()
+    # test.test_add_tasks_consecutively()
+    # test.test_add_task_beggining()
+    # test.test_add_task_middle()
+    # test.test_remove_task_beginning()
+    # test.test_remove_task_middle()
+    # test.test_remove_task_end()
