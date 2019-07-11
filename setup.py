cab71dafe0d70d43eb9eafe8c3fcbbb497e45c1a
Angela Enriquez <anenriqu@outlook.com>
2019-07-11 14:38:00 +0200

    Adding requirements to setup

--- setup.py
+++ setup.py
@@ -4,6 +4,13 @@ from setuptools import setup
 
 setup(name='mrta_stn',
       version='0.1.0',
+      install_requires=[
+            'numpy',
+            'networkx',
+            'PuLP',
+            'scipy',
+            'pyYAML'
+      ],
       description='Includes simple temporal_networks like STN, STNU and PSTN',
       author='Angela Enriquez Gomez',
       author_email='angela.enriquez@smail.inf.h-brs.de',
