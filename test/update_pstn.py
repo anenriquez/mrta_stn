8c51a1fce6fdddca241868161c88f8d771039793
Angela Enriquez <anenriqu@outlook.com>
2019-07-16 10:30:51 +0200

    Removing dependency to mrta_datasets Task information for the tests is added to the data folder There is still a dependency to mrta_allocation to have access to the Task struct

--- test/update_pstn.py
+++ test/update_pstn.py
@@ -1,27 +1,29 @@
-import unittest
-import logging
-import sys
 from stn.pstn.pstn import PSTN
-from datasets.dataset_loader import load_dataset
+import unittest
+import os
+import json
+from allocation.task import Task
 
-
-logger = logging.getLogger()
-logger.level = logging.INFO
-stream_handler = logging.StreamHandler(sys.stdout)
-logger.addHandler(stream_handler)
+code_dir = os.path.abspath(os.path.dirname(__file__))
+TASKS = code_dir + "/data/three_tasks.json"
 
 
 class TestBuildPSTN(unittest.TestCase):
-    logger = logging.getLogger('stn.test')
 
     def setUp(self):
-        self.tasks = load_dataset('three_tasks.csv')
+        with open(TASKS) as json_file:
+            tasks_dict = json.load(json_file)
+
+        self.tasks = list()
+        for task_dict in tasks_dict['tasks']:
+            task = Task.from_dict(task_dict)
+            self.tasks.append(task)
 
     def test_add_tasks_consecutively(self):
         """ Adds tasks in consecutive positions. Example
         position 1, position 2, ...
         """
-        self.logger.info("--->Adding tasks consecutively...")
+        print("--->Adding tasks consecutively...")
         pstn = PSTN()
 
         for i, task in enumerate(self.tasks):
@@ -30,9 +32,9 @@ class TestBuildPSTN(unittest.TestCase):
         n_nodes = 3 * len(self.tasks) + 1
         n_edges = 2 * (5 * len(self.tasks) + len(self.tasks)-1)
 
-        self.logger.info("N nodes: %s ", n_nodes)
-        self.logger.info("N edges:%s ", n_edges)
-        self.logger.info(pstn)
+        print("N nodes: ", n_nodes)
+        print("N edges: ", n_edges)
+        print(pstn)
 
         self.assertEqual(n_nodes, pstn.number_of_nodes())
         self.assertEqual(n_edges, pstn.number_of_edges())
@@ -40,53 +42,53 @@ class TestBuildPSTN(unittest.TestCase):
     def test_add_task_beggining(self):
         """Adds task at the beginning. Displaces the other tasks
         """
-        self.logger.info("--->Adding task at the beginning...")
+        print("--->Adding task at the beginning...")
         pstn = PSTN()
         # Adds the first task in position 1
         pstn.add_task(self.tasks[0], 1)
-        self.logger.info("%s", pstn)
+        print(pstn)
 
         # Adds a new task in position 1. Displaces existing task at position 1 to position 2
         pstn.add_task(self.tasks[1], 1)
-        self.logger.info("%s", pstn)
+        print(pstn)
 
         # We added two tasks
         added_tasks = [self.tasks[0], self.tasks[1]]
         n_nodes = 3 * len(added_tasks) + 1
         n_edges = 2 * (5 * len(added_tasks) + len(added_tasks)-1)
 
-        self.logger.info("N nodes: %s ", n_nodes)
-        self.logger.info("N edges: %s ", n_edges)
-        self.logger.info(pstn)
+        print("N nodes: ", n_nodes)
+        print("N edges: ", n_edges)
+        print(pstn)
 
         self.assertEqual(n_nodes, pstn.number_of_nodes())
         self.assertEqual(n_edges, pstn.number_of_edges())
 
     def test_add_task_middle(self):
-        self.logger.info("--->Adding task in the middle...")
+        print("--->Adding task in the middle...")
         pstn = PSTN()
         # Add task in position 1
         pstn.add_task(self.tasks[0], 1)
 
         # Adds task in position 2.
         pstn.add_task(self.tasks[1], 2)
-        self.logger.info("%s", pstn)
+        print(pstn)
 
         # Add a new task in position 2. Displace existing task at position 2 to position 3
         pstn.add_task(self.tasks[2], 2)
-        self.logger.info("%s", pstn)
+        print(pstn)
 
         n_nodes = 3 * len(self.tasks) + 1
         n_edges = 2 * (5 * len(self.tasks) + len(self.tasks)-1)
 
-        self.logger.info("N nodes: %s ", n_nodes)
-        self.logger.info("N edges: %s", n_edges)
+        print("N nodes: ", n_nodes)
+        print("N edges: ", n_edges)
 
         self.assertEqual(n_nodes, pstn.number_of_nodes())
         self.assertEqual(n_edges, pstn.number_of_edges())
 
     def test_remove_task_beginning(self):
-        self.logger.info("--->Removing task at the beginning...")
+        print("--->Removing task at the beginning...")
         pstn = PSTN()
 
         # Add all tasks
@@ -100,21 +102,21 @@ class TestBuildPSTN(unittest.TestCase):
         n_nodes = 3 * len(added_tasks) + 1
         n_edges = 2 * (5 * len(added_tasks) + len(added_tasks)-1)
 
-        self.logger.info("N nodes: %s", n_nodes)
-        self.logger.info("N edges: %s ", n_edges)
-        self.logger.info(pstn)
+        print("N nodes: ", n_nodes)
+        print("N edges: ", n_edges)
+        print(pstn)
 
         self.assertEqual(n_nodes, pstn.number_of_nodes())
         self.assertEqual(n_edges, pstn.number_of_edges())
 
     def test_remove_task_middle(self):
-        self.logger.info("--->Removing task in the middle...")
+        print("--->Removing task in the middle...")
         pstn = PSTN()
 
         # Add all tasks
         for i, task in enumerate(self.tasks):
             pstn.add_task(task, i+1)
-        self.logger.info("%s", pstn)
+        print(pstn)
 
         # Remove task in position 2
         pstn.remove_task(2)
@@ -123,24 +125,24 @@ class TestBuildPSTN(unittest.TestCase):
         n_nodes = 3 * len(added_tasks) + 1
         n_edges = 2 * (5 * len(added_tasks) + len(added_tasks)-1)
 
-        self.logger.info("N nodes: %s ", n_nodes)
-        self.logger.info("N edges: %s", n_edges)
-        self.logger.info(pstn)
+        print("N nodes: ", n_nodes)
+        print("N edges: ", n_edges)
+        print(pstn)
 
 
         self.assertEqual(n_nodes, pstn.number_of_nodes())
         self.assertEqual(n_edges, pstn.number_of_edges())
 
     def test_remove_task_end(self):
-        self.logger.info("--->Removing task at the end...")
+        print("--->Removing task at the end...")
         pstn = PSTN()
 
         # Add all tasks
         for i, task in enumerate(self.tasks):
             pstn.add_task(task, i+1)
-            self.logger.info(pstn)
+            print(pstn)
 
-        self.logger.info(pstn)
+        print(pstn)
         # Remove task in position 3
         pstn.remove_task(3)
 
@@ -148,9 +150,9 @@ class TestBuildPSTN(unittest.TestCase):
         n_nodes = 3 * len(added_tasks) + 1
         n_edges = 2 * (5 * len(added_tasks) + len(added_tasks)-1)
 
-        self.logger.info("N nodes: %s", n_nodes)
-        self.logger.info("N edges: %s", n_edges)
-        self.logger.info(pstn)
+        print("N nodes: ", n_nodes)
+        print("N edges: ", n_edges)
+        print(pstn)
 
         self.assertEqual(n_nodes, pstn.number_of_nodes())
         self.assertEqual(n_edges, pstn.number_of_edges())
--- test/update_stnu.py
+++ test/update_stnu.py
@@ -1,27 +1,29 @@
 from stn.stnu.stnu import STNU
 import unittest
-import logging
-import sys
-from datasets.dataset_loader import load_dataset
+import os
+import json
+from allocation.task import Task
 
-
-logger = logging.getLogger()
-logger.level = logging.INFO
-stream_handler = logging.StreamHandler(sys.stdout)
-logger.addHandler(stream_handler)
+code_dir = os.path.abspath(os.path.dirname(__file__))
+TASKS = code_dir + "/data/three_tasks.json"
 
 
 class UpdateSTNU(unittest.TestCase):
-    logger = logging.getLogger('stn.test')
 
     def setUp(self):
-        self.tasks = load_dataset('three_tasks.csv')
+        with open(TASKS) as json_file:
+            tasks_dict = json.load(json_file)
+
+        self.tasks = list()
+        for task_dict in tasks_dict['tasks']:
+            task = Task.from_dict(task_dict)
+            self.tasks.append(task)
 
     def test_add_tasks_consecutively(self):
         """ Adds tasks in consecutive positions. Example
         position 1, position 2, ...
         """
-        self.logger.info("--->Adding tasks consecutively...")
+        print("--->Adding tasks consecutively...")
         stnu = STNU()
 
         for i, task in enumerate(self.tasks):
@@ -30,9 +32,9 @@ class UpdateSTNU(unittest.TestCase):
         n_nodes = 3 * len(self.tasks) + 1
         n_edges = 2 * (5 * len(self.tasks) + len(self.tasks)-1)
 
-        self.logger.info("N nodes: %s ", n_nodes)
-        self.logger.info("N edges: %s ", n_edges)
-        self.logger.info(stnu)
+        print("N nodes: ", n_nodes)
+        print("N edges: ", n_edges)
+        print(stnu)
 
         self.assertEqual(n_nodes, stnu.number_of_nodes())
         self.assertEqual(n_edges, stnu.number_of_edges())
@@ -40,53 +42,53 @@ class UpdateSTNU(unittest.TestCase):
     def test_add_task_beggining(self):
         """Adds task at the beginning. Displaces the other tasks
         """
-        self.logger.info("--->Adding task at the beginning...")
+        print("--->Adding task at the beginning...")
         stnu = STNU()
         # Adds the first task in position 1
         stnu.add_task(self.tasks[0], 1)
-        self.logger.info("%s", stnu)
+        print(stnu)
 
         # Adds a new task in position 1. Displaces existing task at position 1 to position 2
         stnu.add_task(self.tasks[1], 1)
-        self.logger.info("%s", stnu)
+        print(stnu)
 
         # We added two tasks
         added_tasks = [self.tasks[0], self.tasks[1]]
         n_nodes = 3 * len(added_tasks) + 1
         n_edges = 2 * (5 * len(added_tasks) + len(added_tasks)-1)
 
-        self.logger.info("N nodes: %s ", n_nodes)
-        self.logger.info("N edges: %s", n_edges)
-        self.logger.info(stnu)
+        print("N nodes: ", n_nodes)
+        print("N edges: ", n_edges)
+        print(stnu)
 
         self.assertEqual(n_nodes, stnu.number_of_nodes())
         self.assertEqual(n_edges, stnu.number_of_edges())
 
     def test_add_task_middle(self):
-        self.logger.info("--->Adding task in the middle...")
+        print("--->Adding task in the middle...")
         stnu = STNU()
         # Add task in position 1
         stnu.add_task(self.tasks[0], 1)
 
         # Adds task in position 2.
         stnu.add_task(self.tasks[1], 2)
-        self.logger.info("%s", stnu)
+        print(stnu)
 
         # Add a new task in position 2. Displace existing task at position 2 to position 3
         stnu.add_task(self.tasks[2], 2)
-        self.logger.info("%s", stnu)
+        print(stnu)
 
         n_nodes = 3 * len(self.tasks) + 1
         n_edges = 2 * (5 * len(self.tasks) + len(self.tasks)-1)
 
-        self.logger.info("N nodes: %s", n_nodes)
-        self.logger.info("N edges: %s", n_edges)
+        print("N nodes: ", n_nodes)
+        print("N edges: ", n_edges)
 
         self.assertEqual(n_nodes, stnu.number_of_nodes())
         self.assertEqual(n_edges, stnu.number_of_edges())
 
     def test_remove_task_beginning(self):
-        self.logger.info("--->Removing task at the beginning...")
+        print("--->Removing task at the beginning...")
         stnu = STNU()
 
         # Add all tasks
@@ -100,21 +102,21 @@ class UpdateSTNU(unittest.TestCase):
         n_nodes = 3 * len(added_tasks) + 1
         n_edges = 2 * (5 * len(added_tasks) + len(added_tasks)-1)
 
-        self.logger.info("N nodes: %s", n_nodes)
-        self.logger.info("N edges: %s", n_edges)
-        self.logger.info(stnu)
+        print("N nodes: ", n_nodes)
+        print("N edges: ", n_edges)
+        print(stnu)
 
         self.assertEqual(n_nodes, stnu.number_of_nodes())
         self.assertEqual(n_edges, stnu.number_of_edges())
 
     def test_remove_task_middle(self):
-        self.logger.info("--->Removing task in the middle...")
+        print("--->Removing task in the middle...")
         stnu = STNU()
 
         # Add all tasks
         for i, task in enumerate(self.tasks):
             stnu.add_task(task, i+1)
-        self.logger.info(stnu)
+        print(stnu)
 
         # Remove task in position 2
         stnu.remove_task(2)
@@ -123,23 +125,23 @@ class UpdateSTNU(unittest.TestCase):
         n_nodes = 3 * len(added_tasks) + 1
         n_edges = 2 * (5 * len(added_tasks) + len(added_tasks)-1)
 
-        self.logger.info("N nodes: %s", n_nodes)
-        self.logger.info("N edges: %s", n_edges)
-        self.logger.info(stnu)
+        print("N nodes: ", n_nodes)
+        print("N edges: ", n_edges)
+        print(stnu)
 
         self.assertEqual(n_nodes, stnu.number_of_nodes())
         self.assertEqual(n_edges, stnu.number_of_edges())
 
     def test_remove_task_end(self):
-        self.logger.info("--->Removing task at the end...")
+        print("--->Removing task at the end...")
         stnu = STNU()
 
         # Add all tasks
         for i, task in enumerate(self.tasks):
             stnu.add_task(task, i+1)
-            self.logger.info("%s", stnu)
+            print(stnu)
 
-        self.logger.info("%s", stnu)
+        print(stnu)
         # Remove task in position 3
         stnu.remove_task(3)
 
@@ -147,25 +149,25 @@ class UpdateSTNU(unittest.TestCase):
         n_nodes = 3 * len(added_tasks) + 1
         n_edges = 2 * (5 * len(added_tasks) + len(added_tasks)-1)
 
-        self.logger.info("N nodes: %s ", n_nodes)
-        self.logger.info("N edges: %s", n_edges)
-        self.logger.info("%s", stnu)
+        print("N nodes: ", n_nodes)
+        print("N edges: ", n_edges)
+        print(stnu)
 
         self.assertEqual(n_nodes, stnu.number_of_nodes())
         self.assertEqual(n_edges, stnu.number_of_edges())
 
     def test_add_two_tasks(self):
-        self.logger.info("----Adding two tasks")
+        print("----Adding two tasks")
         stnu = STNU()
         # Add task in position 1
         stnu.add_task(self.tasks[1], 1)
 
         # Adds task in position 2.
         stnu.add_task(self.tasks[0], 2)
-        self.logger.info("%s", stnu)
+        print(stnu)
 
         stnu_json = stnu.to_json()
-        self.logger.info("JSON format % s", stnu_json)
+        print("JSON format ", stnu_json)
 
 
 if __name__ == '__main__':
