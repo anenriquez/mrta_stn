6ec8c12653277d741d681f8bbe7f45c04e4178c0
Angela Enriquez <anenriqu@outlook.com>
2019-09-26 09:41:25 +0200

    setup: New release version 0.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(name='stn',
       packages=['stn', 'stn.config', 'stn.methods', 'stn.pstn', 'stn.stnu', 'stn.utils'],
-      version='0.1.0',
+      version='0.2.0',
       install_requires=[
             'numpy',
             'networkx',
