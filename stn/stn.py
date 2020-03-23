6d5823ad888fa54e8943dfddd0a1e4a3e81f4fcf
Ángela Enríquez Gómez <anenriqu@outlook.com>
2020-03-23 09:29:25 +0100

    stn: Add methods get_task_nodes and get_task_graph

--- stn/stn.py
+++ stn/stn.py
@@ -540,6 +540,22 @@ class STN(nx.DiGraph):
 
         self.logger.debug("STN has no tasks yet")
 
+    def get_task_nodes(self, task_id):
+        """ Gets the nodes in the stn associated with the given task_id
+
+        Args:
+            task_id: (string) id of the task
+
+        Returns: list of node ids
+
+        """
+        nodes = list()
+        for i in self.nodes():
+            if task_id == self.nodes[i]['data'].task_id:
+                nodes.append(self.nodes[i]['data'])
+
+        return nodes
+
     def get_task_node_ids(self, task_id):
         """ Gets the node_ids in the stn associated with the given task_id
 
@@ -580,6 +596,21 @@ class STN(nx.DiGraph):
         # The first node in the subgraph is the zero timepoint
         node_ids.insert(0, 0)
 
+        sub_graph = self.subgraph(node_ids)
+        return sub_graph
+
+    def get_task_graph(self, task_id):
+        """ Returns a graph with the nodes of the task_id
+
+        Args:
+            task_id: ID of the task
+
+        Returns: nx graph with nodes of task_id
+
+        """
+        node_ids = self.get_task_node_ids(task_id)
+        # The first node in the subgraph is the zero timepoint
+        node_ids.insert(0, 0)
         sub_graph = self.subgraph(node_ids)
         return sub_graph
 
